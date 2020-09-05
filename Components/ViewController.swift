55fc3a4e6892d226a7b687f19a427b2c0466cc58
Andrew Finnell <andy@losingfight.com>
2020-09-05 11:28:23 -0400

    Initial Commit

---
+++ Components/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Components
+//
+//  Created by Andrew Finnell on 9/5/20.
+//  Copyright © 2020 Safe from the Losing Fight. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
