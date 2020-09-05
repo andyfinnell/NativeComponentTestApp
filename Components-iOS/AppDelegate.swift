7cada09890401c146438ed2c05b3669dd2e4f66c
Andrew Finnell <andy@losingfight.com>
2020-09-05 11:48:03 -0400

    Create a BannerKit framework target for macOS and iOS

--- Components-iOS/AppDelegate.swift
+++ Components-iOS/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import BannerKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- Components/AppDelegate.swift
+++ Components/AppDelegate.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import BannerKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
