d8b495a2f31bfdeb9012d4c2c27ba8a05dd5c125
Andrew Finnell <andy@losingfight.com>
2020-10-03 13:44:03 -0400

    Switch dependencies to use HTTPS instead of SSH

--- Modules/BannerKit/Package.swift
+++ Modules/BannerKit/Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "git@github.com:andyfinnell/ViewKit.git", from: "0.0.1"),
-        .package(url: "git@github.com:andyfinnell/NativeComponentKit.git", .branch("master"))
+        .package(url: "https://github.com/andyfinnell/ViewKit.git", from: "0.0.4"),
+        .package(url: "https://github.com/andyfinnell/NativeComponentKit.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
